d9759573d3d7a0bf593ac2368966df27446f3608
Roy <royrmagdaong@gmail.com>
2023-12-04 17:48:50 +0800

    add login, register, blog-post, users pages

---
+++ src/app/login/page.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import NavBar from '../components/nav-bar'
+
+export default function Login() {
+  return (
+    <div>
+      <NavBar />
+      <h1>Login</h1>
+    </div>
+  )
+}
---
+++ src/app/register/page.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import NavBar from '../components/nav-bar'
+
+export default function Register() {
+  return (
+    <div>
+      <NavBar />
+      <h1>Register</h1>
+    </div>
+  )
+}
