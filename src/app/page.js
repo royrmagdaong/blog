2763fb6dad0b18c6324c480d7a3986d2afb0031d
Roy <royrmagdaong@gmail.com>
2023-12-04 17:42:26 +0800

    add navbar

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,10 @@
 import React from 'react'
+import NavBar from './components/nav-bar'
 
 export default function Home() {
   return (
-    <div className='h-screen flex justify-center items-center'>
-      <h1 className='text-2xl'>Home</h1>
+    <div>
+      <NavBar />
     </div>
   )
 }
